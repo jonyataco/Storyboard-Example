cc87e2dfce1905374cf393d4188a3b6f7b1fdfd6
Jonathan Yataco <jonathan.yataco@bombbomb.com>
2022-08-26 14:37:17 -0400

    Initial commit

--- BraedensFirstStoryboard/ViewController.swift
+++ BraedensFirstStoryboard/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var TopLabel: UILabel!
+    @IBAction func Button(_ sender: Any) {
+        TopLabel.text = "Button pressed"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTopLabel()
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    // Whenever the view controllers view size will change
+    // On orientation change
+    // When window resizes maybe in stage manager or splitscreen
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+    }
+    
+    func setupTopLabel() {
+        TopLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            TopLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            TopLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 50)
+        ])
+    }
+}
 
+class CustomView: UIView {
+    
 }
 
